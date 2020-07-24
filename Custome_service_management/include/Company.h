88f1dea3e32072ad7cdd10efde99fe11de86d1ca
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-24 16:53:11 +0600

    Add files via upload

---
+++ Custome_service_management/include/Company.h
@@ -0,0 +1,26 @@
+#ifndef COMPANY_H
+#define COMPANY_H
+
+#include<string>
+#include"customer.h"
+#include"employee.h"
+
+class Company
+{
+    public:
+        Company(std::string name);
+        ~Company();
+        bool add(Customer c,int EmployeeIndex);
+        void showStatus();
+        bool addEmployee(Employee e);
+        bool served(int customerID);
+    protected:
+    private:
+        Employee employees[100];
+        int nextSerial;
+        int totalEmployee;
+        int totalCustomer;
+        std::string name;
+};
+
+#endif // COMPANY_H
