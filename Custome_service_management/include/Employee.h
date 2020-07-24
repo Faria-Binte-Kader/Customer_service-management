c06f7064e38ec7562ef0707b517d6ce3f4f654bc
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-24 16:54:44 +0600

    Update Employee.h

--- Custome_service_management/include/Employee.h
+++ Custome_service_management/include/Employee.h
@@ -5,24 +5,35 @@
 
 
 class Employee : public Person
-{
+{   protected:
+        string experties;
+        int experience;
+        string worksas;
+        double salary;
+        int workingfor;
+        int employee_id;
+        static int id;
+        Customer customer[10];
     public:
-        Employee();
+        Employee(string nam,long long int phn,string addr,string em,string exp,int ex,string work);
         virtual ~Employee();
 
         string Getexperties() { return experties; }
         void Setexperties(string val) { experties = val; }
+        int Getexperience() { return experience; }
+        void Setexperience(int val) { experience = val; }
         string Getworksas() { return worksas; }
         void Setworksas(string val) { worksas = val; }
         double Getsalary() { return salary; }
         void Setsalary(double val) { salary = val; }
-
-    protected:
-
-    private:
-        string experties;
-        string worksas;
-        double salary;
+        void addCustomer(Customer c);
+        bool removeCustomer(int index);
+        void getEmployee_id();
+        void setEmployeeId(int id);
+        int getSalary();
+        double calcsalary();
+        //int getGroupId();  //implement later
+        //void password_generator();
 };
 
 #endif // EMPLOYEE_H
