6d697cdc121174a5fb60710d8b491139eb1312ba
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-28 00:43:15 +0600

    Update Person.cpp

--- Custome_service_management/src/Person.cpp
+++ Custome_service_management/src/Person.cpp
@@ -1,4 +1,5 @@
 #include "Person.h"
+#include<iostream>
 
 Person::Person(string nam,long long int phn,string addr,string em):name(""),phone_no(0),address(""),email("")
 {
@@ -7,9 +8,26 @@ Person::Person(string nam,long long int phn,string addr,string em):name(""),phon
     Setphone_no(phn);
     Setaddress(addr);
     Setemail(em);
+    password=new char[1];
+    strcpy(password,"");
 }
 
 Person::~Person()
 {
     //dtor
+    delete[] password;
+}
+void Person::Setpassword(char *c)
+        {
+            delete[] password;
+            password=new char[strrlen(c)+1];
+            strcpy(paasword,c);
+        }
+
+void Person::dispalyinfo()
+{
+    cout<<"Name: "<<Getname()<<endl;
+    cout<<"Phone number: "<<Getphone_no()<<endl;
+    cout<<"City: "<<Getaddress()<<endl;
+    cout<<"Email: "<<Getemail()<<endl;
 }
