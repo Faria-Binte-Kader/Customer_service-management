4bea01b302d92e8aaf7b9aef0d01eec4d17ecae6
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-29 04:18:59 +0600

    Update Customer.h

--- Custome_service_management/include/Customer.h
+++ Custome_service_management/include/Customer.h
@@ -1,28 +1,93 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
+#include"Person.h"
 
-#include <Person.h>
+using namespace std;
 
-
-class Customer : public Person
+struct Date
 {
-    public:
-        Customer();
-        virtual ~Customer();
-
-        int Gettime() { return time; }
-        void Settime(int val) { time = val; }
-        double Getbudget() { return budget; }
-        void Setbudget(double val) { budget = val; }
-        string Getrequirement() { return requirement; }
-        void Setrequirement(string val) { requirement = val; }
-
-    protected:
-
-    private:
-        int time;
-        double budget;
-        string requirement;
+    int day;
+    int month;
+    //string month;
+    int year;
+    Date() {}
+    Date(int d,int m,int y):day(d),month(m),year(y) {}
 };
 
-#endif // CUSTOMER_H
+enum class WorkType {Web_Development,Mobile_Development,Data_Science,Application_Development,Embedded_Systems,Cloud_Computing};
+class Customer:virtual public Person
+{
+protected:
+    double budget;
+    int time;
+    double final_cost;
+    string description;
+    //int project_id;
+    Date assigned_date;
+    WorkType work;
+public:
+    static int cust_id;
+    Customer();
+    Customer(string n,string ph,string ad,string em,double b,int t,string desc,Date d,WorkType w);
+    virtual ~Customer();
+    double GetBudget()
+    {
+        return budget;
+    }
+    void SetBudget(double val)
+    {
+        budget=val;
+    }
+    int GetTime()
+    {
+        return time;
+    }
+    void SetTime(int val)
+    {
+        time=val;
+    }
+    int GetCost()
+    {
+        return final_cost;
+    }
+    void SetCost(int val)
+    {
+        final_cost=val;
+    }
+    string GetDescription()
+    {
+        return description;
+    }
+    void SetDescription(string val)
+    {
+        description=val;
+    }
+    /*int GetProjectId()
+    {
+        return project_id;
+    }
+    void SetProjectId(int val)
+    {
+        project_id=val;
+    }*/
+
+    Date GetDate()
+    {
+        return assigned_date;
+    }
+    void SetDate(Date val)
+    {
+        assigned_date=val;
+    }
+    string getWork();
+    void SetWork(WorkType val)
+    {
+        work=val;
+    }
+    bool isServed(Date dt);
+    void generatePassword();
+    void displayinfo();
+};
+int Customer::cust_id=1000;
+#endif
+
