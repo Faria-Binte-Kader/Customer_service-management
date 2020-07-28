cae8b8d41bd4eeac033a4b7522856c4994ef06f1
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-29 04:20:05 +0600

    Update Employee.h

--- Custome_service_management/include/Employee.h
+++ Custome_service_management/include/Employee.h
@@ -1,39 +1,72 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
-
 #include <Person.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+enum class Designation {Web_Development,Mobile_Development,Data_Science,Application_Development,Embedded_Systems,Cloud_Computing};
+
+class Employee : virtual public Person
+{
+protected:
+    Designation designation;
+    int experience;
+    string position;
+    double salary;
+    static int employee_id;
+    int no_of_projects;
+    double bonus;
+    //Customer customer[10];
+public:
+    Employee();
+    Employee(string nam,string phn,string addr,string em,Designation des,int ex,double salary,int nopro);
+    virtual ~Employee();
+
+    string Getdesignation();
+    void Setdesignation(Designation val)
+    {
+        designation = val;
+    }
+    int Getexperience()
+    {
+        return experience;
+    }
+    void Setexperience(int val)
+    {
+        experience = val;
+    }
+    string Getposition()
+    {
+        return position;
+    }
+    void Setposition(int val);
+    double Getsalary()
+    {
+        return salary;
+    }
+    void Setsalary(double val)
+    {
+        salary = val;
+    }
+    //void addCustomer(Customer c);
+    //bool removeCustomer(int index);
+    void getEmployee_id();
+    static void setEmployeeId(int id)
+    {
+        employee_id=id;
+    };
+    double getbonus();
+    void calcbonus();
+    int getno_ofprojects();
+    void generatePassword();
+    void displayinfo();
+
+    //int getGroupId();  //implement later
 
 
-class Employee : public Person
-{   protected:
-        string experties;
-        int experience;
-        string worksas;
-        double salary;
-        int workingfor;
-        int employee_id;
-        static int id;
-        Customer customer[10];
-    public:
-        Employee(string nam,long long int phn,string addr,string em,string exp,int ex,string work);
-        virtual ~Employee();
 
-        string Getexperties() { return experties; }
-        void Setexperties(string val) { experties = val; }
-        int Getexperience() { return experience; }
-        void Setexperience(int val) { experience = val; }
-        string Getworksas() { return worksas; }
-        void Setworksas(string val) { worksas = val; }
-        double Getsalary() { return salary; }
-        void Setsalary(double val) { salary = val; }
-        void addCustomer(Customer c);
-        bool removeCustomer(int index);
-        void getEmployee_id();
-        void setEmployeeId(int id);
-        int getSalary();
-        double calcsalary();
-        //int getGroupId();  //implement later
-        //void password_generator();
 };
 
+int Employee::employee_id=0;
 #endif // EMPLOYEE_H
