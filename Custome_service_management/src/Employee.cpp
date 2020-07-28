d153b92af88f3e57b9e2ba08a5d46b80d61c2d3e
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-29 04:22:02 +0600

    Update Employee.cpp

--- Custome_service_management/src/Employee.cpp
+++ Custome_service_management/src/Employee.cpp
@@ -1,13 +1,25 @@
 #include "Employee.h"
+#include<bits/stdc++.h>
+using namespace std;
 
-Employee::Employee(string nam,long long int phn,string addr,string em,string exp,int ex,string work):Person(nam,phn,addr,em),experience(""),experience(0),worksas("")
+Employee::Employee():Person(),designation(Designation::Web_Development),experience(0),salary(0.0),no_of_projects(0)
 {
     //ctor
-    id++;
-    Setexperties(exp);
+
+    employee_id++;
+    setEmployeeId(employee_id);
+    generatePassword();
+}
+
+Employee::Employee(string nam,string phn,string addr,string em,Designation des,int ex,double salary,int nopro):Person(nam,phn,addr,em),designation(Designation::Web_Development),experience(0),salary(0.0),no_of_projects(0)
+{
+    //ctor
+    employee_id++;
+    Setdesignation(des);
     Setexperience(ex);
-    Setworksas(work);
-    setEmployeeId(id);
+    setEmployeeId(employee_id);
+    Setposition(ex);
+    generatePassword();
 }
 
 Employee::~Employee()
@@ -15,7 +27,26 @@ Employee::~Employee()
     //dtor
 }
 
-void Employee::addCustomer(Customer c)
+
+
+string Employee::Getdesignation()
+{
+    if(designation==Designation::Web_Development)
+        return string("Web Development");
+    else if(designation==Designation::Mobile_Development)
+        return string("Mobile Development");
+    else if(designation==Designation::Data_Science)
+        return string("Data Science");
+    else if(designation==Designation::Application_Development)
+        return string("Application Development");
+    else if(designation==Designation::Embedded_Systems)
+        return string("Embedded Systems");
+    else if(designation==Designation::Cloud_Computing)
+        return string("Cloud Computing");
+    else
+        return string("");
+}
+/*void Employee::addCustomer(Customer c)
 {
     if(workingfor+1>10) cout<<"Occupied"<<endl;
     else
@@ -26,16 +57,62 @@ void Employee::addCustomer(Customer c)
     }
 bool Employee::removeCustomer(int index)
 {
+
     if(index+1>workingfor||index<0) return false;
     else if(index+1==workingfor)
     {
         workingfor--;
         return true;
     }
+}*/
+
+void Employee::Setposition(int a)
+{
+    if(a>=3)
+        position="Junior group 1";
+    else if(a<=6 && a>3)
+        position="Junior group 2";
+    else if(a>6)
+        position="Senior";
 }
-	/*double Employee::calcSalary(int experience)
-	{
 
+void Employee::displayinfo()
+{
+    Person::displayinfo();
+    cout<<Getdesignation()<<endl;
+    cout<<Getexperience()<<endl;
+    cout<<Getposition()<<endl;
+    cout<<Getsalary()<<endl;
+    //cout<<getno_ofprojects()<<endl;
 
+}
+void Employee::generatePassword()
+{
+    delete [] password;
+    char lower[]="abcdefghijklmnopqrstuvwxyz";
+    char upper[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char special[]="!@#$%&-_(),:{}";
+    char number[]="0123456789";
+    password= new char[15];
+    int b=0;
+    for(int i=0; i<5; i++)
+    {
+        password[b]=upper[rand()%26];
+        b++;
+    }
+    for(int i=0; i<4; i++)
+    {
+        password[b]=lower[rand()%26];
+        b++;
+    }
+    password[b]=number[rand()%10];
+    b++;
+    for(int i=0; i<2; i++)
+    {
+        password[b]=special[rand()%18];
+        b++;
+    }
+    std::random_shuffle(password,password+12);
+    password[12]='\0';
 
-	}*/
+}
