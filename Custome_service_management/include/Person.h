1f9e5da2d5d5a1a41c637d5443c13afe171046b7
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-29 04:26:21 +0600

    Update Person.h

--- Custome_service_management/include/Person.h
+++ Custome_service_management/include/Person.h
@@ -2,27 +2,28 @@
 #define PERSON_H
 #include<iostream>
 
-
+using namespace std;
 class Person
 {
     public:
-        Person(string nam,long long int phn,string addr,string em);
+        Person();
+        Person(string nam,string phn,string addr,string em);
         virtual ~Person();
 
         string Getname() { return name; }
         void Setname(string val) { name = val; }
-        long long int Getphone_no() { return phone_no; }
-        void Setphone_no(long long int val) { phone_no = val; }
+        string Getphone_no() { return phone_no; }
+        void Setphone_no(string val) { phone_no = val; }
         string Getaddress() { return address; }
         void Setaddress(string val) { address = val; }
         string Getemail() { return email; }
         void Setemail(string val) { email = val; }
-        char* Getpassword() { return *password; }
+        char* Getpassword() { return password; }
         void displayinfo();
         virtual void generatePassword() = 0;
     protected:
         string name;
-        long long int phone_no;
+        string phone_no;
         string address;
         string email;
         char *password;
@@ -32,4 +33,5 @@ class Person
 };
 
 #endif // PERSON_H
+
 
