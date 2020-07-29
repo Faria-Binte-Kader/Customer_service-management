3a0bdf9e5bc69a132a64cfb668d2f554d598c5fd
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-29 18:17:39 +0600

    Update Person.h

--- Custome_service_management/include/Person.h
+++ Custome_service_management/include/Person.h
@@ -1,37 +1,56 @@
 #ifndef PERSON_H
 #define PERSON_H
-#include<iostream>
-
-using namespace std;
+#include<bits/stdc++.h>
 class Person
 {
-    public:
-        Person();
-        Person(string nam,string phn,string addr,string em);
-        virtual ~Person();
-
-        string Getname() { return name; }
-        void Setname(string val) { name = val; }
-        string Getphone_no() { return phone_no; }
-        void Setphone_no(string val) { phone_no = val; }
-        string Getaddress() { return address; }
-        void Setaddress(string val) { address = val; }
-        string Getemail() { return email; }
-        void Setemail(string val) { email = val; }
-        char* Getpassword() { return password; }
-        void displayinfo();
-        virtual void generatePassword() = 0;
-    protected:
-        string name;
-        string phone_no;
-        string address;
-        string email;
-        char *password;
-
-        void Setpassword(char *c);
-
+protected:
+    std::string name;
+    std::string phone_no;
+    std::string address;
+    std::string email;
+    char *password;
+    void SetPassword(char *c);
+public:
+    Person();
+    Person(std::string nam,std::string phn,std::string addr,std::string em);
+    virtual ~Person();
+    std::string GetName()
+    {
+        return name;
+    }
+    void SetName(std::string val)
+    {
+        name=val;
+    }
+    std::string GetPhone_no()
+    {
+        return phone_no;
+    }
+    void SetPhone_no(std::string val)
+    {
+        phone_no=val;
+    }
+    std::string GetAddress()
+    {
+        return address;
+    }
+    void SetAddress(std::string val)
+    {
+        address=val;
+    }
+    std::string GetEmail()
+    {
+        return email;
+    }
+    void SetEmail(std::string val)
+    {
+        email=val;
+    }
+    char* GetPassword()
+    {
+        return password;
+    }
+    virtual void generatePassword()=0;
+    void displayinfo();
 };
-
-#endif // PERSON_H
-
-
+#endif
