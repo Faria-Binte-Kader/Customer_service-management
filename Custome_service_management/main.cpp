4f339f1b6b013995f3abc8a7d7d02220173f3c56
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-29 23:22:30 +0600

    Update main.cpp

--- Custome_service_management/main.cpp
+++ Custome_service_management/main.cpp
@@ -1,59 +1,53 @@
 #include"Customer.h"
+#include"Employee.h"
+#include"Student.h"
 #include<ctime>
+#include<string>
 #include<bits/stdc++.h>
 using namespace std;
-void receiveInfo(Customer &c1)
+Customer custReceiveInfo(Customer &c)
 {
-    string nam,phn,addr,em,desc;
+    string nam,phn,addr,em,desc,garbage;
     double b;
     int t,wk;
     WorkType w;
     cout<<"Enter your name: ";
     getline(cin,nam);
-    cout<<endl;
     cout<<"Enter your contact no.: ";
-    cin>>phn;
-    cout<<endl;
+    getline(cin,phn);
     cout<<"Enter your address: ";
     getline(cin,addr);
-    cout<<endl;
     cout<<"Enter your email: ";
     cin>>em;
-    cout<<endl;
     cout<<"Enter your budget: ";
     cin>>b;
-    cout<<endl;
     cout<<"Enter the time(in months 1-24): ";
     cin>>t;
-    cout<<endl;
-    cout<<"Enter the type(0.Web_Development/1.Mobile_Development/2.Data_Science/3.Application_Development/4.Embedded_Systems/5.Cloud_Computing): ";
+    cout<<"Enter the type:\n0.Web Development\n1.Mobile Development\n2.Data Science\n3.Application Development\n4.Embedded Systems\n5.Cloud Computing"<<endl;
     cin>>wk;
     w=static_cast<WorkType>(wk);
-    cout<<endl;
+    getline(cin,garbage);
     cout<<"Enter description: ";
     getline(cin,desc);
-    cout<<endl;
     Date dt2;
     time_t now=time(0);
     tm* dt1=localtime(&now);
     dt2.day=dt1->tm_mday;
-    dt2.month=dt1->tm_mon;
-    dt2.year=dt1->tm_year;
-//    c1.SetName(nam);
-//    c1.SetPhone_no(phn);
-//    c1.SetAddress(addr);
-//    c1.SetEmail(em);
-//    c1.setBudget(b);
-//    c1.setDuration(t);
-//    c1.setDescription(desc);
-//    c1.setDate(dt2);
-//    c1.setWork(w);
-    c1(nam,phn,addr,em,b,t,desc,dt2,w);
+    dt2.month=1+dt1->tm_mon;
+    dt2.year=1900+dt1->tm_year;
+    c.setinfo(nam,phn,addr,em,b,t,desc,dt2,w);
+    return c;
 }
 int main()
 {
     Customer c1;
-    receiveInfo(c1);
+    c1=custReceiveInfo(c1);
     c1.displayinfo();
+    Customer c2("a b c","00000","d e","aaa",50000,3,"f g",Date(1,2,2020),WorkType::Data_Science);
+    //c2=custReceiveInfo(c2);
+    c2.displayinfo();
+    Customer c3;
+    c3=custReceiveInfo(c3);
+    c3.displayinfo();
     return 0;
 }
