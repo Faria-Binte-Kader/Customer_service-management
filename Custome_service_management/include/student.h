d4430a288c58fb8b1e44e7065e2082f7c9dd65d9
WrightlyRong <55374565+WrightlyRong@users.noreply.github.com>
2020-07-30 02:56:02 +0600

    Add files via upload

--- Custome_service_management/include/student.h
+++ Custome_service_management/include/student.h
@@ -1,9 +1,12 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-#include<Person.h>
-#include<bits/stdc++.h>
-enum class Available_Courses {Web_Development,Data_Science,Application_Development,Embedded_Systems,Cloud_Computing};
-class Student:virtual public Person
+
+#include <Person.h>
+
+
+enum class Available_Courses {Web_Development,Mobile_Development,Data_Science,Application_Development,Embedded_systems,Cloud_Computing};
+
+class Student :virtual public Person
 {
 protected:
     Available_Courses course;
@@ -11,6 +14,9 @@ protected:
     Date starting_date;
     int course_duration;
     std::string grade;
+
+    const long long int student_id=200031001+total_students+1;//starts from 31001 because student is our 3rd class xD
+    static int total_students; //if student_id creates any problem it's because of this one
 public:
     Student();
     Student(std::string nam,std::string phn,std::string addr,std::string em,std::string cors,double corsfe, Date strtdat,int drtn,std::string grd);
@@ -21,7 +27,7 @@ public:
     {
         return course_fee;
     }
-    void setCourseFee(int cf)
+    void setCourseFee(double cf)
     {
         course_fee=cf;
     }
@@ -46,18 +52,18 @@ public:
         return grade;
     }
     void setGrade(std::string g);
-    void dispalyinfo();
     bool isQualified();
     void generatePassword();
     char* getPassword();
     void displayinfo();
+
+    void setInfo(std::string nam,std::string phn,std::string addr,std::string em,std::string cors,double corsfe, Date strtdat,int drtn,std::string grd);
     bool hasCompleted();
-    void setStudentId(Available_Courses c);
+    //void setStudentId(Available_Courses c);
     long long int getStudentId()
     {
         return student_id;
     }
-    long long int student_id=200031001+total_students+1;//starts from 31001 because student is our 3rd class xD
-    static int total_students; //if student_id creates any problem it's because of this one
 };
-#endif
+
+#endif // STUDENT_H
