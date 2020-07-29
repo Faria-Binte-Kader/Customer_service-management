3d5b17e6ffa04d064dc9531d2af06427a42d053c
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-29 20:22:19 +0600

    Update student.h

--- Custome_service_management/include/student.h
+++ Custome_service_management/include/student.h
@@ -10,18 +10,18 @@ class Student :virtual public Person
 {
 public:
     Student();
-    Student(string nam,string phn,string addr,string em,string cors,double corsfe, Date strtdat,int drtn,string grd);
+    Student(std::string nam,std::string phn,std::string addr,std::string em,std::string cors,double corsfe, Date strtdat,int drtn,std::string grd);
     virtual ~Student();
-    string getCourse();
-    void setCourse(string c);
+    std::string getCourse();
+    void setCourse(std::string c);
     double getCourseFee(){return course_fee;}
     void setCourseFee(int cf){course_fee=cf;}
     Date getStartingDate(){return starting_date;}
     void setStartingDate(Date st){starting_date=st;}
     int getCourseDuration(){return course_duration;}
     void setCourseDuration(int cd){course_duration=cd;}
-    string getGrade(){return grade;}
-    void setGrade(string g);
+    std::string getGrade(){return grade;}
+    void setGrade(std::string g);
     void dispalyinfo();
     bool isQualified();
     void generatePassword();
@@ -37,7 +37,7 @@ protected:
     double course_fee;
     Date starting_date;
     int course_duration;
-    string grade;
+    std::string grade;
 
 
     long long int student_id=200031001+total_students+1;//starts from 31001 because student is our 3rd class xD
