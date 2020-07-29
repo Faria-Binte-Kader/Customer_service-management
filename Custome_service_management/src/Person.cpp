b05d8b6f9f4aeecfa497dac8a7319aa2efec77d2
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-29 23:20:30 +0600

    Update Person.cpp

--- Custome_service_management/src/Person.cpp
+++ Custome_service_management/src/Person.cpp
@@ -1,15 +1,15 @@
 #include"Person.h"
 #include<bits/stdc++.h>
-Person::Person():name(""),phone_no(0),address(""),email("")
+Person::Person():name(""),phone_no(""),address(""),email("")
 {
 
 }
 Person::Person(std::string nam,std::string phn,std::string addr,std::string em):name(nam),phone_no(phn),address(addr),email(em)
 {
-    SetName(nam);
-    SetPhone_no(phn);
-    SetAddress(addr);
-    SetEmail(em);
+    setName(nam);
+    setPhone_no(phn);
+    setAddress(addr);
+    setEmail(em);
     password=new char[1];
     strcpy(password,"");
 }
@@ -17,7 +17,7 @@ Person::~Person()
 {
     delete[] password;
 }
-void Person::SetPassword(char *c)
+void Person::setPassword(char *c)
 {
     delete[] password;
     password=new char[strlen(c)+1];
@@ -25,8 +25,8 @@ void Person::SetPassword(char *c)
 }
 void Person::displayinfo()
 {
-    std::cout<<"Name: "<<GetName()<<std::endl;
-    std::cout<<"Phone number: "<<GetPhone_no()<<std::endl;
-    std::cout<<"Address: "<<GetAddress()<<std::endl;
-    std::cout<<"Email: "<<GetEmail()<<std::endl;
+    std::cout<<"Name: "<<getName()<<std::endl;
+    std::cout<<"Phone number: "<<getPhone_no()<<std::endl;
+    std::cout<<"Address: "<<getAddress()<<std::endl;
+    std::cout<<"Email: "<<getEmail()<<std::endl;
 }
