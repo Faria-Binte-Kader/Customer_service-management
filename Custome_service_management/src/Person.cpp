c7f7262e7cda9f090c1df4b8d218d74bbe867f50
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-29 18:20:03 +0600

    Update Person.cpp

--- Custome_service_management/src/Person.cpp
+++ Custome_service_management/src/Person.cpp
@@ -1,36 +1,32 @@
-#include "Person.h"
+#include"Person.h"
 #include<bits/stdc++.h>
-using namespace std;
-
-Person::Person()
-{}
-Person::Person(string nam,string phn,string addr,string em):name(""),phone_no(0),address(""),email("")
+Person::Person():name(""),phone_no(0),address(""),email("")
 {
-    //ctors
-    Setname(nam);
-    Setphone_no(phn);
-    Setaddress(addr);
-    Setemail(em);
+
+}
+Person::Person(std::string nam,std::string phn,std::string addr,std::string em):name(nam),phone_no(phn),address(addr),email(em)
+{
+    SetName(nam);
+    SetPhone_no(phn);
+    SetAddress(addr);
+    SetEmail(em);
     password=new char[1];
     strcpy(password,"");
 }
-
 Person::~Person()
 {
-    //dtor
     delete[] password;
 }
-void Person::Setpassword(char *c)
-        {
-            delete[] password;
-            password=new char[strlen(c)+1];
-            strcpy(password,c);
-        }
-
+void Person::SetPassword(char *c)
+{
+    delete[] password;
+    password=new char[strlen(c)+1];
+    strcpy(password,c);
+}
 void Person::displayinfo()
 {
-    cout<<"Name: "<<Getname()<<endl;
-    cout<<"Phone number: "<<Getphone_no()<<endl;
-    cout<<"City: "<<Getaddress()<<endl;
-    cout<<"Email: "<<Getemail()<<endl;
+    std::cout<<"Name: "<<GetName()<<std::endl;
+    std::cout<<"Phone number: "<<GetPhone_no()<<std::endl;
+    std::cout<<"Address: "<<GetAddress()<<std::endl;
+    std::cout<<"Email: "<<GetEmail()<<std::endl;
 }
