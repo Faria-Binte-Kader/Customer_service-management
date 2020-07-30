413b1de2f81f8b39b562c0a73a48c6d750392814
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-30 17:22:50 +0600

    Update Customer.h

--- Custome_service_management/include/Customer.h
+++ Custome_service_management/include/Customer.h
@@ -1,6 +1,6 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
-#include<Person.h>
+#include"Person.h"
 #include<bits/stdc++.h>
 enum class WorkType{Web_Development,Mobile_Development,Data_Science,Application_Development,Embedded_Systems,Cloud_Computing};
 class Customer:virtual public Person
@@ -14,7 +14,8 @@ protected:
     Date assigned_date;
     WorkType work;
 public:
-    static int cust_id;
+    long long int cust_id=1000+total_customer;
+    static int total_customer;
     Customer();
     Customer(std::string nam,std::string phn,std::string addr,std::string em,double b,int t,std::string desc,Date d,WorkType w);
     virtual ~Customer();
