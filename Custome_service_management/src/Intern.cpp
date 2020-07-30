1f8bd529c9e6256442d058d0fa0e88178a1c024b
Faria-Binte-Kader <55392622+Faria-Binte-Kader@users.noreply.github.com>
2020-07-31 05:23:44 +0600

    Add files via upload

---
+++ Custome_service_management/src/Intern.cpp
@@ -0,0 +1,45 @@
+#include "Intern.h"
+#include<bits/stdc++.h>
+
+Intern::Intern():Person(),Student(),Employee(),intership_period(0)
+{
+    //ctor
+    generatePassword();
+    total_interns++;
+}
+
+int Intern::total_interns=0;
+
+void Intern::generatePassword()
+{
+    Employee::generatePassword();
+}
+
+void Intern::displayinfo()
+{
+      std::cout<<"Name: "<<Student::getName()<<std::endl;
+      std::cout<<"Phone number: "<<Student::getPhone_no()<<std::endl;
+      std::cout<<"Address: "<<Student::getAddress()<<std::endl;
+      std::cout<<"Email: "<<Student::getEmail()<<std::endl;
+      //std::cout<<"Password "<<Employee::password<<std::endl;
+      std::cout<<"Intern id: "<<getInternid()<<std::endl;
+      std::cout<<"Internship period: "<<getPeriod()<<std::endl;
+      std::cout<<"Designation: "<<Student::getCourse()<<std::endl;
+      //std::cout<<"Projectid: "<<getProjectid()<<std::endl;
+
+}
+
+void Intern::setinfo(std::string nam,std::string phn,std::string addr,std::string em,std::string cors,int period)
+{
+     Student::setName(nam);
+     Student::setPhone_no(phn);
+     Student::setAddress(addr);
+     Student::setEmail(em);
+     Student::setCourse(cors);
+     setPeriod(period);
+}
+
+Intern::~Intern()
+{
+    //dtor
+}
