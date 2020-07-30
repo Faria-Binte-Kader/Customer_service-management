6afab2bfa119de91a14494294ec355b330546053
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-07-30 18:03:56 +0600

    Update Employee.h

--- Custome_service_management/include/Employee.h
+++ Custome_service_management/include/Employee.h
@@ -1,6 +1,6 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
-#include<Person.h>
+#include"Person.h"
 #include<bits/stdc++.h>
 enum class Designation{Web_Development,Mobile_Development,Data_Science,Application_Development,Embedded_Systems,Cloud_Computing};
 class Employee:virtual public Person
@@ -10,11 +10,12 @@ protected:
     int experience;
     std::string position;
     double salary;
+    static int total_employee;
+    int employee_id=2000+total_employee;
     //int no_of_projects;
     //double bonus;
     //Customer customer[10];
 public:
-    static int employee_id;
     Employee();
     Employee(std::string nam,std::string phn,std::string addr,std::string em,std::string des,int ex);
     virtual ~Employee();
@@ -38,6 +39,11 @@ public:
         return salary;
     }
     void setSalary();
+    int getEmployeeID()
+    {
+        return employee_id;
+    }
+    void setEmployeeID(Designation d);
     //void addCustomer(Customer c);
     //bool removeCustomer(int index);
     /*double getbonus()
