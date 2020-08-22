a2129d76b0cd950f1c5f19b7cc40a30a936e1c6a
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-08-22 13:30:32 +0600

    Update Company.h

--- Custome_service_management/include/Company.h
+++ Custome_service_management/include/Company.h
@@ -2,25 +2,25 @@
 #define COMPANY_H
 
 #include<string>
-#include"customer.h"
-#include"employee.h"
+#include"Customer.h"
+#include"Employee.h"
 
 class Company
 {
-    public:
-        Company(std::string name);
-        ~Company();
-        bool add(Customer c,int EmployeeIndex);
-        void showStatus();
-        bool addEmployee(Employee e);
-        bool served(int customerID);
-    protected:
-    private:
-        Employee employees[100];
-        int nextSerial;
-        int totalEmployee;
-        int totalCustomer;
-        std::string name;
+public:
+    Company(std::string name);
+    ~Company();
+    bool add(Customer c,int EmployeeIndex);
+    void showStatus();
+    bool addEmployee(Employee e);
+    bool served(int customerID);
+protected:
+private:
+    Employee employees[100];
+    int nextSerial;
+    int totalEmployee;
+    int totalCustomer;
+    std::string name;
 };
 
 #endif // COMPANY_H
