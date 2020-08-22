cc3de7f621aaeec9e5a54be56d38e3525d29e60f
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-08-22 15:21:35 +0600

    Update Project.h

--- Custome_service_management/include/Project.h
+++ Custome_service_management/include/Project.h
@@ -7,30 +7,48 @@
 #include<ctime>
 class Project
 {
-    protected:
-        static int total_projects;
-        const int projectId=10001+total_projects;
-        Employee emp1,emp2,emp3;
-        Customer cust;
-        int project_time; //in months
-        std::string description;
-        Date delivery_date;
-        std::string status;
-        double cost;
-    public:
-        Project();
-        Project(Customer &c, Employee &e1,Employee &e2, Employee &e3, double cst, int tm, std::string des);
-        virtual ~Project();
-        int getProjectId(){return projectId;}
-        int getProjectTime(){return project_time;}
-        void setStatus();
-        std::string getStatus(){return status;}
-        Date getDeliveryDate(){return delivery_date;}
-        Date calcDeliveryDate();
-        void setCost(double c) { cost=c; }
-        double getCost(){return cost;}
-        void setInfo(Customer &c, Employee &e1,Employee &e2, Employee &e3,double cst);
-        void projectDetails();
+protected:
+    static int total_projects;
+    const int projectId=10001+total_projects;
+    Employee emp1,emp2,emp3;
+    Customer cust;
+    int project_time; //in months
+    std::string description;
+    Date delivery_date;
+    std::string status;
+    double cost;
+public:
+    Project();
+    Project(Customer &c, Employee &e1,Employee &e2, Employee &e3, double cst, int tm, std::string des);
+    virtual ~Project();
+    int getProjectId()
+    {
+        return projectId;
+    }
+    int getProjectTime()
+    {
+        return project_time;
+    }
+    void setStatus();
+    std::string getStatus()
+    {
+        return status;
+    }
+    Date getDeliveryDate()
+    {
+        return delivery_date;
+    }
+    Date calcDeliveryDate();
+    void setCost(double c)
+    {
+        cost=c;
+    }
+    double getCost()
+    {
+        return cost;
+    }
+    void setInfo(Customer &c, Employee &e1,Employee &e2, Employee &e3,double cst);
+    void projectDetails();
 };
 
 #endif // PROJECT_H
