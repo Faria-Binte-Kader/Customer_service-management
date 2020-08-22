37927055059bf484e3ad01a95f0564ed960cbfa9
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-08-22 14:56:19 +0600

    Update Company.cpp

--- Custome_service_management/src/Company.cpp
+++ Custome_service_management/src/Company.cpp
@@ -1,73 +1,44 @@
 #include"Company.h"
 #include<iostream>
-
-Company::Company(std::string name):nextSerial(1),totalEmployee(0),totalCustomer(0)
+Company::Company():name(""),totalEmployee(0),totalCustomer(0)
 {
-    //ctor
-    this->name=name;
+    total_company++;
 }
-
+Company::Company(std::string nam):name(nam),totalEmployee(0),totalCustomer(0)
+{
+    total_company++;
+    setName(nam);
+}
 Company::~Company()
 {
-    //dtor
-}
 
-bool Company::add(Customer c,int EmployeeIndex)
+}
+int Company::total_company=0;
+void Company::addCustomer(Customer c)
 {
-    if(EmployeeIndex>totalEmployee) return false;
-    else
-    {
-        c.SetcustomerId(nextSerial++);
-        if(!employees[EmployeeIndex].addCustomer(c)) return false;
-        else
-        {
-            totalCustomer++;
-            return true;
-        }
-    }
+    customers[totalCustomer]=c;
+    totalCustomer++;
 }
-
+void Company::addEmployee(Employee e)
+{
+    employees[totalEmployee]=e;
+    totalEmployee++;
+}
 void Company::showStatus()
 {
-    std::cout<<name<<std::endl;
-    std::cout<<"Total Added Customer: "<<totalCustomer<<std::endl;
+    std::cout<<"Name: "<<name<<std::endl;
+    std::cout<<"ID: "<<company_id<<std::endl;
+    std::cout<<"Total Customer: "<<totalCustomer<<std::endl;
     std::cout<<"Total Employee: "<<totalEmployee<<std::endl;
-    std::cout<<"-----"<<std::endl;
-    for(int i=0; i<totalEmployee; i++)
+    std::cout<<"------------------------------"<<std::endl;
+    for(int i=0;i<totalEmployee;i++)
     {
-        std::cout<<employees[i].Getname()<<" has following customers\n";
-        for(int j=0; j<employees[i].GetnoOfCustomerTakenCare(); j++)
-        {
-            std::cout<<employees[i].getCustomerName(j)<<std::endl;
-        }
-        if(i+1<totalEmployee)std::cout<<"-----"<<std::endl;
+        employees[i].displayinfo();
+        std::cout<<std::endl;
     }
-
-}
-bool Company::addEmployee(Employee e)
-{
-    if(totalEmployee+1>100) return false;
-    else
+    for(int i=0;i<totalCustomer;i++)
     {
-        employees[totalEmployee++]=e;
-        return true;
+        customers[i].displayinfo();
+        std::cout<<std::endl;
     }
-}
-bool Company::served(int customerID)
-{
-    for(int i=0; i<totalEmployee; i++)
-    {
-        for(int j=0; j<employees[i].GetnoOfCustomerTakenCare(); j++)
-        {
-            if(employees[i].getCustomer(j).GetcustomerId()==customerID)
-            {
-                if(employees[i].removeCustomer(j))
-                {
-                    totalCustomer--;
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
 }
