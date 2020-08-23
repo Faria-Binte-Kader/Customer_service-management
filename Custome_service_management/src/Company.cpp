59ad9f14570f6ebfa34ced4bffb6acd69dea5e12
nhnujhat <55582093+nhnujhat@users.noreply.github.com>
2020-08-23 17:47:04 +0600

    Update Company.cpp

--- Custome_service_management/src/Company.cpp
+++ Custome_service_management/src/Company.cpp
@@ -3,19 +3,16 @@
 #include<fstream>
 Company::Company():name(""),totalEmployee(0),totalCustomer(0)
 {
-    total_company++;
-    //std::cout<<"Total company"<<total_company<<std::endl;
+
 }
 Company::Company(std::string nam):name(nam),totalEmployee(0),totalCustomer(0)
 {
-    total_company++;
-    //std::cout<<"Total company"<<total_company<<std::endl;
     setName(nam);
 }
 Company::~Company()
 {
+
 }
-int Company::total_company=0;
 void Company::addCustomer(Customer &c)
 {
     customers[totalCustomer]=&c;
@@ -48,14 +45,36 @@ void Company::addEmployee(Employee &e)
     }}*/
 
 
-void Company::editProjectid(int employeeid1,int employeeid2,int employeeid3,int projectid,int customerindex,int cst)
+void Company::editProjectid(Customer &c1,Employee &e1,Employee &e2,Employee &e3,int pid)
 {
-    employees[employeeid1]->setProjectId(projectid);
-    employees[employeeid2]->setProjectId(projectid);
-    employees[employeeid3]->setProjectId(projectid);
-    customers[customerindex]->SetProjectId(projectid);
-    customers[customerindex]->setCost(cst);
-
+    int cnt=0;
+    for(int i=0;i<totalEmployee;i++)
+    {
+        if(e1.getEmployeeID()==employees[i]->getEmployeeID())
+        {
+            e1.setProjectId(pid);
+            cnt++;
+        }
+        if(e2.getEmployeeID()==employees[i]->getEmployeeID())
+        {
+            e2.setProjectId(pid);
+            cnt++;
+        }
+        if(e3.getEmployeeID()==employees[i]->getEmployeeID())
+        {
+            e3.setProjectId(pid);
+            cnt++;
+        }
+        if(cnt==3) break;
+    }
+    for(int i=0;i<totalCustomer;i++)
+    {
+        if(c1.getCustID()==customers[i]->getCustID())
+        {
+            c1.SetProjectId(pid);
+            break;
+        }
+    }
 }
 
 void Company::updateEmployeelist()
@@ -150,4 +169,3 @@ void Company::showCustomer()
     }
     fin.close();
 }
-
